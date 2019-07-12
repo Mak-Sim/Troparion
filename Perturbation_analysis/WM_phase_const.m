ad1c3b01c2f0719647b1c9c478e9868aa90aab75
Maksim Vashkevich <vashkevich.m@gmail.com>
2019-07-12 19:12:50 +0300

    WM method changed.

--- Perturbation_analysis/WM_phase_const.m
+++ Perturbation_analysis/WM_phase_const.m
@@ -6,6 +6,15 @@ F0 = MyFit(time_marks*fs,F0,time_line);
 F0 = F0/fs*2*pi;
 T0 = zeros(size(F0));
 
+% % a = lpc(sig,6);
+% % [h,w]=freqz(1,a,128,fs);
+% % m = abs(h);
+% % m = m/max(m);
+% % h_size = 60;
+% % b = fir2(2*h_size,[w(1:end-1)/(fs/2); 1],m);
+% % sig = filter(b,1,[sig; zeros(h_size,1)]);
+% % sig = sig(h_size+1:end);
+
 % Initialization
 Ln = length(sig);
 cur_cum = 0;
@@ -36,11 +45,21 @@ while (n<(Ln-T0(Nc)))
 %         bias=-offset:offset;
         bias=round(-offset*0.8:offset*1.2);
         for k=1:length(bias)
-            T_cur = sig(n-1-T0(Nc)+1+bias(k):n-1+bias(k));            
-            err(k) = mean(abs(T_prev - T_cur));
-%             err(k) = mean((T_prev - T_cur).^2);
+            front_edge = n-1+bias(k);
+            if (bias(k)>=0)
+                Tc = T0(Nc);
+                back_edge = front_edge - Tc + 1;
+                T_cur = sig(back_edge:front_edge);
+            else
+                Tc = T0(Nc)+bias(k);
+                back_edge = front_edge - Tc + 1;
+                T_cur = sig(back_edge:front_edge);
+            end
+            err(k) = mean(abs(T_prev(end-Tc+1:end) - T_cur));
         end
         [~,min_ind] = min(err);
+        
+% %         % Infographics
 % %         subplot(311);
 % %         plot(bias,err);
 % %         hold on;        
@@ -53,21 +72,14 @@ while (n<(Ln-T0(Nc)))
 % %         hold off;
 % %         pause(1/20);
 % %         drawnow();
+        
         n = n - 1 + bias(min_ind);       
         Nc = Nc + 1;        
         T0(Nc) = cur_P+bias(min_ind);
         T_prev = sig(n-T0(Nc)+1:n);        
-         
-% %         if min_ind==length(bias)
-% %             cur_cum = cur_cum - 2.005*pi; % little overhead
-% %         elseif min_ind==1
-% %             cur_cum = cur_cum - 1.995*pi; % little shortage
-% %         else
-% %             cur_cum = cur_cum - 2*pi; 
-% %         end
+        
         cur_period = 0;
-        cur_cum = 0;
-                 
+        cur_cum = 0;                 
     else
         n = n + 1;
         cur_cum = cur_cum + F0(n);

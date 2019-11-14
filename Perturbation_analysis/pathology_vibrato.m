f8d503ac7b4d8e59b051a8f7ec05ca5f03f3ad4d
Maksim Vashkevich <vashkevich.m@gmail.com>
2019-11-14 11:13:11 +0300

    Small bug fixed in PVI calculation function

--- Perturbation_analysis/pathology_vibrato.m
+++ Perturbation_analysis/pathology_vibrato.m
@@ -1,13 +1,9 @@
 function [pvi,F0_norm,F0_filt,vib_spect,freq] = pathology_vibrato(F0,dt,f_size,hop_size)
-%PATHOLOGY_VIBRATO detection of unusual vibrato
-%   Pathological voicec having to big vibrato
+%PATHOLOGY_VIBRATO -- calculation of PVI (pathological vibrato index)
+%   Pathological voices having to big vibrato
 
 fs = 1/dt;
-% % subplot(211);
-% % plot(F0);
-F0_norm = F0/mean(F0);
-% % subplot(212)
-% % plot(F0);
+F0_norm = F0(:)/mean(F0);
 
 [b,a]=butter(3,[9/(fs/2) 14/(fs/2)]);
 F0_filt = filter(b,a,[F0_norm(100:-1:2); F0_norm]); % extension mirror
@@ -42,17 +38,12 @@ for N=1:frame_num
 %     frame_dm = frame_detr - mean(frame_detr);
 %     plot(frame_dm);
 %     frame = frame - mean(frame);
-    frame_fft = fft(frame_detr.*hamming(f_size)');
+    frame_fft = fft(frame_detr.*hamming(f_size));
     buf = buf + abs(frame_fft(1:f_size/2));    
 end
 vib_spect = buf/frame_num;
-% [~,lsor] = findpeaks(vib_spect(1:20),'SortStr','descend','Threshold',1,'NPeaks',1);
-% % [vib_p,~] = findpeaks(vib_spect(1:20),'SortStr','descend','NPeaks',1);
-% % if isempty(vib_p)
-% %     vib_p = max(vib_spect(1:20));
-% % end
 
-pvi = sum(vib_spect(10:15));
+pvi = sum(vib_spect(10:15));    % using only frequencies in range [9 14] Hz
 
 % % plot(freq,vib_spect);
 % % title(['Pathology vibrato = ' num2str(vib_p,"%1.2f") ' ']);
